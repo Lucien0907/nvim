2b44c29ee8e21d8cf94955b27346820c5fe56287
Tao-Wei Chan <taowei.c@outlook.com>
2025-03-03 15:04:23 +0000

    update

--- lua/plugins/lazydocker.lua
+++ lua/plugins/lazydocker.lua
@@ -4,8 +4,19 @@ return {
     "mgierada/lazydocker.nvim",
     dependencies = { "akinsho/toggleterm.nvim" },
     config = function()
-      require("lazydocker").setup({})
+      require("lazydocker").setup({
+        border = "curved", -- valid options are "single" | "double" | "shadow" | "curved"
+      })
     end,
-    event = "BufRead", -- or any other event you might want to use.
+    event = "BufRead",
+    keys = {
+      {
+        "<leader>ld",
+        function()
+          require("lazydocker").open()
+        end,
+        desc = "Open Lazydocker floating window",
+      },
+    },
   },
 }
