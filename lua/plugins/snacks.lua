edfefb1e0e6509d198ddd6144065683ab6bee891
Tao-Wei Chan <taowei.c@outlook.com>
2025-03-05 20:28:58 +0000

    update

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -24,13 +24,21 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      explorer = {},
+      image = {},
+      picker = {},
       dashboard = {
         preset = {
           header = custom_header,
           keys = {
             { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
             -- { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            {
+              icon = " ",
+              key = "c",
+              desc = "Config",
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            },
             -- { icon = " ", key = "s", desc = "Sessions", action = ":lua require('session_manager').load_session(false)" },
             { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
             { icon = " ", key = "q", desc = "Quit", action = ":qa" },
