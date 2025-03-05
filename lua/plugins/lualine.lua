ca1020de21661bdb2e4afd4898d83a6f9985ebe3
Tao-Wei Chan <taowei.c@outlook.com>
2025-03-05 16:14:51 +0000

    update

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,11 +5,11 @@ return {
       options = {
         icons_enabled = true,
         theme = "auto",
-        component_separators = { left = '', right = '' },
+        component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
         disabled_filetypes = {
           statusline = {},
-          winbar = { "neo-tree", "toggleterm" },
+          winbar = { "neo-tree" },
         },
         ignore_focus = {},
         always_divide_middle = true,
