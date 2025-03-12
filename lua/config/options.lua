350f417e7f2376c6a4a20bc09c9029a35a606443
Tao-Wei Chan <taowei.c@outlook.com>
2025-03-12 10:55:39 +0000

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,4 +7,5 @@ opt.scrolloff = 12 -- Lines of context
 opt.winminwidth = 10 -- Minimum window width
 opt.wrap = true -- Disable line wrap
 
+-- Add any additional options here
 vim.g.python3_host_prog = "/home/tao-wei/.pyenv/versions/pynvim/bin/python"
