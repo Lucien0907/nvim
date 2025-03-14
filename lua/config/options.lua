7c679e18f2273ec4980e3fe0aed03a75629bc9f6
Tao-Wei Chan <taowei.c@outlook.com>
2025-03-14 14:36:44 +0000

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,4 +8,5 @@ opt.winminwidth = 10 -- Minimum window width
 opt.wrap = true -- Disable line wrap
 
 -- Add any additional options here
-vim.g.python3_host_prog = "/Users/tao-wei/.pyenv/versions/pynvim/bin/python"
+local home = os.getenv("HOME")
+vim.g.python3_host_prog = home .. "/.pyenv/versions/pynvim/bin/python"
